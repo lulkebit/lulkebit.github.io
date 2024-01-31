86e0e1ae607e8ef085716de633bae2cdd3b182ba
Luke <64534456+lulkebit@users.noreply.github.com>
2024-01-31 09:21:46 +0100

    Updated UI and made some code optimitzations

--- StapesClassGenerator/js/app.js
+++ StapesClassGenerator/js/app.js
@@ -1,89 +1,131 @@
-const className = document.getElementById("className");
-const constructorCheckbox = document.getElementById("constructorCheckbox");
+const className = document.getElementById('className');
+const constructorCheckbox = document.getElementById('constructorCheckbox');
 
-const parameterList = document.getElementById("parameterList");
-const methodsList = document.getElementById("methodsList");
+const parameterList = document.getElementById('parameterList');
+const methodsList = document.getElementById('methodsList');
 
-const outputDiv = document.getElementById("outputDiv");
-const outputTextArea = document.getElementById("output");
-const copyButton = document.getElementById("copyText");
+const outputDiv = document.getElementById('outputDiv');
+const outputTextArea = document.getElementById('output');
+const copyButton = document.getElementById('copyText');
 
 
-function addParameter() {
-    let listEntry = document.createElement("li");
-    let rowInputDiv = document.createElement("div");
-    rowInputDiv.className = "row justify-content-center";
+function createInputField(type, className, id, name, placeholder) {
+    let inputField = document.createElement('input');
+    inputField.type = type;
+    inputField.className = className;
+    inputField.id = id;
+    inputField.name = name;
+    inputField.placeholder = placeholder;
 
-    let colInputDiv = document.createElement("div");
-    colInputDiv.className = "col-sm-4 p-2";
+    return inputField;
+}
 
-    let inputField = document.createElement("input");
-    inputField.type = "text";
-    inputField.id = "parameterName";
-    inputField.name = "parameterName";
-    inputField.placeholder = "Parametername";
+function createButton(className, innerHTML) {
+    let deleteButton = document.createElement('button');
+    deleteButton.className = className;
+    deleteButton.innerHTML = innerHTML;
+    deleteButton.style = 'background-color: rgb(255, 0, 0); color:white;';
 
-    let deleteButton = document.createElement("button");
-    deleteButton.className = "btn";
-    deleteButton.innerHTML = "Entfernen";
-    deleteButton.style = "background-color: rgb(255, 0, 0); color:white;";
-    deleteButton.addEventListener("click", function () {
+    return deleteButton;
+}
+
+function createMethodInput() {
+    methodsList.appendChild(createInput('methodName', 'methodName', 'Methodenname', 'Bsp.: \'myFunction\'', false));
+}
+
+function addParameterInput() {
+    parameterList.appendChild(createInput('parameterName', 'parameterName', 'Parametername', 'Bsp.: \'myParameter\'', true));
+}
+
+function createInput(id, name, placeholder, innerHTML, generateGetterAndSetter) {
+    let listEntry = document.createElement('li');
+    let rowDiv = document.createElement('div');
+    rowDiv.className = 'row justify-content-center';
+
+    let colDiv = document.createElement('div');
+    colDiv.className = 'col-sm-4 p-2';
+
+    let inputGroupDiv = document.createElement('div');
+    inputGroupDiv.className = 'input-group mb-3';
+
+    let inputGroupSpan = document.createElement('span');
+    inputGroupSpan.className = 'input-group-text';
+    inputGroupSpan.innerHTML = 'Parametername'
+
+    let formFloatingDiv = document.createElement('div');
+    formFloatingDiv.className = 'form-floating';
+
+    let textInput = createInputField('text', 'form-control', id, name, placeholder);
+    let textInputLabel = document.createElement('label');
+    textInputLabel.htmlFor = 'parameterName';
+    textInputLabel.innerHTML = innerHTML;
+
+    let deleteButton = createButton('btn', 'Entfernen');
+    deleteButton.addEventListener('click', function () {
         listEntry.remove();
     });
 
-    let rowCheckBoxDiv = document.createElement("div");
-    rowCheckBoxDiv.className = "row justify-content-center";
+    formFloatingDiv.appendChild(textInput);
+    formFloatingDiv.appendChild(textInputLabel);
 
-    let colCheckBoxDiv = document.createElement("div");
-    colCheckBoxDiv.className = "col-sm-4 p-2";
+    inputGroupDiv.appendChild(inputGroupSpan);
+    inputGroupDiv.appendChild(formFloatingDiv);
 
-    let getterCheckboxDiv = document.createElement("div");
-    getterCheckboxDiv.className = "form-check form-switch form-check-inline";
+    colDiv.appendChild(inputGroupDiv);
+    rowDiv.appendChild(colDiv);
 
-    let getterCheckboxInput = document.createElement("input");
-    getterCheckboxInput.className = "form-check-input";
-    getterCheckboxInput.type = "checkbox";
-    getterCheckboxInput.id = "getterCheckbox";
-    getterCheckboxInput.name = "generateGetter";
+    listEntry.appendChild(rowDiv);
+
+    if (generateGetterAndSetter) {
+        listEntry.appendChild(createGetterAndSetterCheckbox());
+        inputGroupDiv.appendChild(deleteButton)
+    } else {
+        inputGroupDiv.appendChild(deleteButton)
+        colDiv.appendChild(document.createElement('hr'));
+    }
+
+    return listEntry;
+}
+
+function createGetterAndSetterCheckbox() {
+    let rowCheckBoxDiv = document.createElement('div');
+    rowCheckBoxDiv.className = 'row justify-content-center';
+
+    let colCheckBoxDiv = document.createElement('div');
+    colCheckBoxDiv.className = 'col-sm-4 p-2';
+
+    let getterCheckboxDiv = document.createElement('div');
+    getterCheckboxDiv.className = 'form-check form-switch form-check-inline';
+
+    let getterCheckboxInput = createInputField('checkbox', 'form-check-input', 'getterCheckbox', 'generateGetter', '');
     getterCheckboxInput.checked = true;
 
-    let getterCheckboxLabel = document.createElement("label");
-    getterCheckboxLabel.className = "form-check-label";
-    getterCheckboxLabel.htmlFor = "getterCheckbox";
-    getterCheckboxLabel.innerHTML = "getter?";
+    let getterCheckboxLabel = document.createElement('label');
+    getterCheckboxLabel.className = 'form-check-label';
+    getterCheckboxLabel.htmlFor = 'getterCheckbox';
+    getterCheckboxLabel.innerHTML = 'getter?';
 
-    let setterCheckboxDiv = document.createElement("div");
-    setterCheckboxDiv.className = "form-check form-switch form-check-inline";
+    let setterCheckboxDiv = document.createElement('div');
+    setterCheckboxDiv.className = 'form-check form-switch form-check-inline';
 
-    let setterCheckboxInput = document.createElement("input");
-    setterCheckboxInput.className = "form-check-input";
-    setterCheckboxInput.type = "checkbox";
-    setterCheckboxInput.id = "setterCheckbox";
-    setterCheckboxInput.name = "generateSetter";
+    let setterCheckboxInput = createInputField('checkbox', 'form-check-input', 'setterCheckbox', 'generateSetter', '');
     setterCheckboxInput.checked = true;
 
-    let setterCheckboxLabel = document.createElement("label");
-    setterCheckboxLabel.className = "form-check-label";
-    setterCheckboxLabel.htmlFor = "setterCheckbox";
-    setterCheckboxLabel.innerHTML = "setter?";
+    let setterCheckboxLabel = document.createElement('label');
+    setterCheckboxLabel.className = 'form-check-label';
+    setterCheckboxLabel.htmlFor = 'setterCheckbox';
+    setterCheckboxLabel.innerHTML = 'setter?';
 
-    let arrayCheckBoxDiv = document.createElement("div");
-    arrayCheckBoxDiv.className = "form-check form-switch form-check-inline";
+    let arrayCheckBoxDiv = document.createElement('div');
+    arrayCheckBoxDiv.className = 'form-check form-switch form-check-inline';
 
-    let arrayCheckBoxInput = document.createElement("input");
-    arrayCheckBoxInput.className = "form-check-input";
-    arrayCheckBoxInput.type = "checkbox";
-    arrayCheckBoxInput.id = "arrayCheckBox";
-    arrayCheckBoxInput.name = "arrayCheckBox";
+    let arrayCheckBoxInput = createInputField('checkbox', 'form-check-input', 'arrayCheckBox', 'arrayCheckBox', '');
     arrayCheckBoxInput.checked = false;
 
-    let arrayCheckboxLabel = document.createElement("label");
-    arrayCheckboxLabel.className = "form-check-label";
-    arrayCheckboxLabel.htmlFor = "arrayCheckBox";
-    arrayCheckboxLabel.innerHTML = "Array";
-
-    colInputDiv.appendChild(inputField);
-    rowInputDiv.appendChild(colInputDiv);
+    let arrayCheckboxLabel = document.createElement('label');
+    arrayCheckboxLabel.className = 'form-check-label';
+    arrayCheckboxLabel.htmlFor = 'arrayCheckBox';
+    arrayCheckboxLabel.innerHTML = 'Array';
 
     getterCheckboxDiv.appendChild(getterCheckboxInput);
     getterCheckboxDiv.appendChild(getterCheckboxLabel);
@@ -97,68 +139,28 @@ function addParameter() {
     colCheckBoxDiv.appendChild(getterCheckboxDiv);
     colCheckBoxDiv.appendChild(setterCheckboxDiv);
     colCheckBoxDiv.appendChild(arrayCheckBoxDiv);
-    colCheckBoxDiv.appendChild(deleteButton);
-    colCheckBoxDiv.appendChild(document.createElement("hr"));
+    colCheckBoxDiv.appendChild(document.createElement('hr'));
     rowCheckBoxDiv.appendChild(colCheckBoxDiv);
 
-    listEntry.appendChild(rowInputDiv);
-    listEntry.appendChild(rowCheckBoxDiv);
-
-    parameterList.appendChild(listEntry);
-}
-
-function addMethod() {
-    let listEntry = document.createElement("li");
-    let rowDiv = document.createElement("div");
-    rowDiv.className = "row justify-content-center";
-
-    let colDiv = document.createElement("div");
-    colDiv.className = "col-sm-4 p-2";
-
-    let inputField = document.createElement("input");
-    inputField.type = "text";
-    inputField.id = "methodName";
-    inputField.name = "methodName";
-    inputField.placeholder = "Methodenname";
-
-    let deleteButton = document.createElement("button");
-    deleteButton.className = "btn";
-    deleteButton.innerHTML = "Entfernen";
-    deleteButton.style = "background-color: rgb(255, 0, 0); color:white;";
-    deleteButton.addEventListener("click", function () {
-        listEntry.remove();
-    });
-
-    colDiv.appendChild(inputField);
-    colDiv.appendChild(deleteButton);
-    colDiv.appendChild(document.createElement("hr"));
-
-    rowDiv.appendChild(colDiv);
-
-    listEntry.appendChild(rowDiv);
-
-    methodsList.appendChild(listEntry);
+    return rowCheckBoxDiv;
 }
 
 function generate() {
-    if (className.value === "") {
-        window.alert("Bitte geben Sie einen gültigen Klassennamen an!");
+    if (className.value === '') {
+        window.alert('Bitte geben Sie einen gültigen Klassennamen an!');
         return;
     }
 
-    let constructor = "";
-    let parameter = "";
-    let getterAndSetter = "";
-    let methods = "";
-    let parameterList = "";
-    let parameterConstructor = "";
+    let constructor = '';
+    let parameter = '';
+    let getterAndSetter = '';
+    let methods = '';
+    let parameterList = '';
+    let parameterConstructor = '';
 
-    for (let i = 0; i < document.getElementsByName("parameterName").length; i++) {
-        if (!document.getElementsByName("arrayCheckBox").item(i).checked) {
-            parameterList += document.getElementsByName("parameterName").item(i).value + ", ";
-            parameterConstructor += "                this." + document.getElementsByName("parameterName").item(i).value + " = " + document.getElementsByName("parameterName").item(i).value + ";\n";
-        } else {
-            parameterConstructor += "                this." + document.getElementsByName("parameterName").item(i).value + " = [];\n";
+    for (let i = 0; i < document.getElementsByName('parameterName').length; i++) {
+        if (document.getElementsByName('arrayCheckBox').item(i).checked) {
+            parameterConstructor += '\t\t\t\tthis.' + document.getElementsByName('parameterName').item(i).value + ' = [];\n';
         }
     }
 
@@ -166,50 +168,50 @@ function generate() {
     parameterList = parameterList.slice(0, -1);
 
     if (constructorCheckbox.checked === true) {
-        constructor += "            constructor : function(" + parameterList + ") {\n" +
+        constructor += '\t\t\tconstructor : function(' + parameterList + ') {\n' +
             parameterConstructor +
-            "            },\n\n"
+            '\t\t\t},\n\n'
     }
 
-    for (let j = 0; j < document.getElementsByName("parameterName").length; j++) {
-        if(document.getElementsByName("parameterName").item(j).value === "") {
-            window.alert("Geben Sie einen gültigen Parameternamen an!");
+    for (let j = 0; j < document.getElementsByName('parameterName').length; j++) {
+        if(document.getElementsByName('parameterName').item(j).value === '') {
+            window.alert('Geben Sie einen gültigen Parameternamen an!');
             return;
         }
 
-        parameter += "            " + document.getElementsByName("parameterName").item(j).value + ": null,\n";
+        parameter += '\t\t\t' + document.getElementsByName('parameterName').item(j).value + ': null,\n';
 
-        let str = document.getElementsByName("parameterName").item(j).value;
+        let str = document.getElementsByName('parameterName').item(j).value;
         let strUpperCase = str.charAt(0).toUpperCase() + str.slice(1);
 
-        if (document.getElementsByName("generateGetter").item(j).checked) {
-            getterAndSetter += "            get" + strUpperCase + ": function() {\n" +
-                "                return this." + str + ";\n" +
-                "            },\n";
+        if (document.getElementsByName('generateGetter').item(j).checked) {
+            getterAndSetter += '\t\t\tget' + strUpperCase + ': function() {\n' +
+                '\t\t\t\treturn this.' + str + ';\n' +
+                '\t\t\t},\n';
         }
-        if (document.getElementsByName("generateSetter").item(j).checked) {
-            getterAndSetter += "            set" + strUpperCase + ": function(" + str + ") {\n" +
-                "                this." + str + " = " + str + ";\n" +
-                "            },\n";
+        if (document.getElementsByName('generateSetter').item(j).checked) {
+            getterAndSetter += '\t\t\tset' + strUpperCase + ': function(' + str + ') {\n' +
+                '\t\t\t\tthis.' + str + ' = ' + str + ';\n' +
+                '\t\t\t},\n';
         }
     }
 
-    for (let k = 0; k < document.getElementsByName("methodName").length; k++) {
-        if(document.getElementsByName("methodName").item(k).value === "") {
-            window.alert("Geben Sie einen gültigen Methodennamen an!");
+    for (let k = 0; k < document.getElementsByName('methodName').length; k++) {
+        if(document.getElementsByName('methodName').item(k).value === '') {
+            window.alert('Geben Sie einen gültigen Methodennamen an!');
             return;
         }
 
-        methods += "            " + document.getElementsByName("methodName").item(k).value + " : function() {\n" +
-            "                \n" +
-            "            },\n"
+        methods += '\t\t\t' + document.getElementsByName('methodName').item(k).value + ' : function() {\n' +
+            '\t\t\t\t\n' +
+            '\t\t\t},\n'
     }
 
-    let stringPartOne = "define(['tao/lib/stapes'],\n" +
-        "    function(Stapes)\n" +
-        "        'use strict';\n" +
-        "        var " + className.value + " = Stapes.subclass(/** @lends " + className.value + ".prototype */{\n" +
-        parameter + "\n" +
+    let stringPartOne = 'define([\'tao/lib/stapes\'],\n' +
+        '\tfunction(Stapes)\n' +
+        '\t\t\'use strict\';\n' +
+        '\t\tvar ' + className.value + ' = Stapes.subclass(/** @lends ' + className.value + '.prototype */{\n' +
+        parameter + '\n' +
         constructor +
         methods +
         getterAndSetter;
@@ -217,23 +219,23 @@ function generate() {
     stringPartOne = stringPartOne.slice(0, -1);
     stringPartOne = stringPartOne.slice(0, -1);
 
-    let stringPartTwo = "\n       });\n\n" +
-        "       return " + className.value + ";\n" +
-        "   });";
+    let stringPartTwo = '\n\t\t});\n\n' +
+        '\t\treturn ' + className.value + ';\n' +
+        '\t});';
 
     outputTextArea.value = stringPartOne + stringPartTwo;
     outputDiv.hidden = false;
 }
 
 function copy() {
-    navigator.clipboard.writeText(outputTextArea.value).then(r => copyButton.innerHTML = "Erledigt!");
-    setTimeout(() => { copyButton.innerHTML = "Kopieren"; }, 1000);
+    navigator.clipboard.writeText(outputTextArea.value).then(r => copyButton.innerHTML = 'Erledigt!');
+    setTimeout(() => { copyButton.innerHTML = 'Kopieren'; }, 1000);
 }
 
 function download() {
     let element = document.createElement('a');
     element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(outputTextArea.value));
-    element.setAttribute('download', "output");
+    element.setAttribute('download', 'output');
 
     element.style.display = 'none';
     document.body.appendChild(element);
